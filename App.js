40e51c742b7b0dd0668fb31cbd2777cf54988338
Carter Clifford <carterlc@live.ca>
2023-03-12 22:39:20 -0600

    Added two buttons to let the user quickly navigate back to the home page instead of being stuck in an endless cycle of details screens.

--- App.js
+++ App.js
@@ -25,6 +25,8 @@ function DetailsScreen({ navigation }) {
         title="Go to Details... again"
         onPress={() => navigation.push('Details')}
       />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
@@ -35,7 +37,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }}/>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
